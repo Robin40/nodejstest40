879c45e38f11307d6b9efe4fe7615e00bfb94496
Robinson Castro Fernández <Robinson Castro Fernndez>
2016-07-01 03:07:06 -0400

    pdf2text al menos en localhost

--- index.js
+++ index.js
@@ -9,11 +9,44 @@ app.get('/', function(req, res) {
 })
 
 app.get('/api/pdf2text', function(req, res) {
-	res.send("in: " + req.query['in']);
+	var staticDir = __dirname + '/static';
+	var filename = staticDir + '/' + req.query.in + '.pdf';
+	app.use(express.static(staticDir));
+	require('pdfjs-dist');
+	var fs = require('fs');
+	var data = new Uint8Array(fs.readFileSync(filename));
+	
+	var textArray;
+	PDFJS.getDocument(data).then(function (pdf) {
+		textArray = new Array(pdf.numPages);
+		
+		var page2txt = function (pageNum) {
+			return pdf.getPage(pageNum).then(function (page) {
+				return page.getTextContent().then(function (content) {
+					var strings = content.items.map(function (item) {
+						return item.str;
+					});
+					console.log('loadPage ' + pageNum);
+					textArray[pageNum-1] = strings.join(' ');
+				});
+			});
+		};
+		
+		var promiseChain = Promise.resolve();
+		for (var pageNum = 1; pageNum <= pdf.numPages; ++pageNum)
+			promiseChain = promiseChain.then(page2txt.bind(null, pageNum));
+		return promiseChain;
+	})
+	.then(function () {
+		var output = "";
+		output += "El texto es:\n";
+		output += textArray.join('\n----\n');
+		res.send(output);
+	});
 })
 
 app.get('/test/staticfile', function(req, res) {
-	output = "";
+	var output = "";
 	app.use(express.static(__dirname + '/static'));
 	var filename = __dirname + '/static' + '/' + req.query['in'] + '.pdf';
 	//res.sendfile(__dirname + '/static' + '/' + req.query['in'] + '.pdf');
@@ -23,8 +56,13 @@ app.get('/test/staticfile', function(req, res) {
 	var data = new Uint8Array(fs.readFileSync(filename));
 	PDFJS.getDocument(data).then(function (pdfDocument) {
 		output += "Number of pages: " + pdfDocument.numPages + '\n';
+		res.send(output);
 	});
-	res.send(output);
+})
+
+app.get('/test/consolelog', function(req, res) {
+	console.log("le foo");
+	res.send("foo");
 })
 
 app.listen(app.get('port'), function() {
