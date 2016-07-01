fc109f548a84d8ab8d117e484d633a9a105384e1
Robinson Castro Fernández <Robinson Castro Fernndez>
2016-06-30 21:39:53 -0400

    req.query test

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('/', function(req, res) {
+	res.send('Hello World!');
+})
+
+app.get('/api/pdf2text', function(req, res) {
+	res.send("in: " + req.query['in']);
 })
 
 app.listen(app.get('port'), function() {
