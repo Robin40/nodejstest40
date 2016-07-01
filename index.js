a36c165fd4198689a85c8232e5d2a0510531e865
Robinson Castro Fernández <Robinson Castro Fernndez>
2016-06-30 22:02:07 -0400

    staticfile test

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.get('/api/pdf2text', function(req, res) {
 	res.send("in: " + req.query['in']);
 })
 
+app.get('/test/staticfile', function(req, res) {
+	app.use(express.static(__dirname + '/static'));
+	res.sendfile(__dirname + '/static' + '/' + req.query['in'] + '.pdf');
+	res.end;
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
