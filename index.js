11de2f7a9c826410a85ff17673fe76e4226c65dc
Robinson Castro Fernández <Robinson Castro Fernndez>
2016-06-30 22:09:43 -0400

    require test

--- index.js
+++ index.js
@@ -13,9 +13,18 @@ app.get('/api/pdf2text', function(req, res) {
 })
 
 app.get('/test/staticfile', function(req, res) {
+	output = "";
 	app.use(express.static(__dirname + '/static'));
-	res.sendfile(__dirname + '/static' + '/' + req.query['in'] + '.pdf');
-	res.end;
+	var filename = __dirname + '/static' + '/' + req.query['in'] + '.pdf';
+	//res.sendfile(__dirname + '/static' + '/' + req.query['in'] + '.pdf');
+	//res.end;
+	require('pdfjs-dist');
+	var fs = require('fs');
+	var data = new Uint8Array(fs.readFileSync(filename));
+	PDFJS.getDocument(data).then(function (pdfDocument) {
+		output += "Number of pages: " + pdfDocument.numPages + '\n';
+	});
+	res.send(output);
 })
 
 app.listen(app.get('port'), function() {
